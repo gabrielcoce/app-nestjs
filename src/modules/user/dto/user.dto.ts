dca4f32adff017001a2dd26b539a7673a3b03da9
gcoce <gabrielcoce@outlook.com>
2023-05-05 22:32:58 -0600

    feat: se crean paths de pruebas

--- src/modules/user/dto/user.dto.ts
+++ src/modules/user/dto/user.dto.ts
@@ -1,12 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, MinLength } from 'class-validator';
 import { Role } from 'src/modules/auth/enums/role.enum';
 
 export class UserDto {
   id?: string;
+  @ApiProperty()
   @IsEmail()
   email: string;
+
+  @ApiProperty()
   @MinLength(8)
   password: string;
+
+  @ApiProperty()
   role: Role[];
 
   constructor(id: string, email: string, password: string, role: Role[]) {
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDto } from './dto/user.dto';
-import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorator/roles.decorator';
 import { Role } from '../auth/enums/role.enum';
@@ -18,10 +17,27 @@ export class UserController {
     return await this.userService.createUserSvc(user);
   }
 
+  // @Roles(Role.User)
+  // @UseGuards(JwtAuthGuard, RolesGuard)
+  // @Get('find-user/:email')
+  // async findUser(@Param('email') email: string) {
+  //   return await this.userService.findUserSvc(email);
+  // }
+  @Get('content/server')
+  server() {
+    return 'servidor arriba';
+  }
+
   @Roles(Role.User)
   @UseGuards(JwtAuthGuard, RolesGuard)
-  @Get('find-user/:email')
-  async findUser(@Param('email') email: string) {
-    return await this.userService.findUserSvc(email);
+  @Get('content/usuario')
+  user() {
+    return 'usuario contenido';
+  }
+  @Roles(Role.Admin)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('content/administrador')
+  admin() {
+    return 'admin contenido';
   }
 }
